f0b461b1d35c073470958924880609c790f71d16
BarelySmooth <83728060+BarelySmooth@users.noreply.github.com>
2024-08-29 22:04:39 +0530

    Move placeholder data to separate JSON file

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,23 @@ import React, { useState, createContext, useEffect } from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Board from "./components/Board/Board";
 import "./App.css";
+import placeholderData from "./placeholderData.json";
 
 export const AppContext = createContext();
 
 function App() {
+  /* Here's the hierarchy:
+      -- Dynamic Lists
+
+      -- User's Lists
+         |-- Group Lists
+             |-- Sub Lists (can exist under group as well as standalone)
+  
+  */
+
   //TODO: Change placeholder value to actual state (loaded from localstorage)
-  const [appState, setAppState] = useState({
-    todos: [],
-    currentSelectedTab: {
-      tabType: "dynamic", // either dynamic or userList
-      id: "today",
-    },
-  });
+  const [appState, setAppState] = useState(placeholderData);
+
   const providerValue = { appState, setAppState };
 
   useEffect(() => {
